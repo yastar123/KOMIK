068ed7aa2c5cab1c32b8b2101cc257dea86e6002
yastar123 <eduj2164@gmail.com>
2025-04-14 21:19:07 +0700

    commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,7 @@ import { AuthProvider } from '../context/AuthContext';
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <meta name="google-site-verification" content="kode-verifikasi-anda" />
       <Component {...pageProps} />
     </AuthProvider>
   );
