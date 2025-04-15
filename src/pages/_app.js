a30eb546e9d96e51e6f8ad629ac848f682f1bcc1
yastar123 <eduj2164@gmail.com>
2025-04-15 21:19:30 +0700

    commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import { AuthProvider } from '../context/AuthContext';
 import Head from 'next/head';
+import Script from 'next/script'; // <-- Tambahkan ini
 
 export default function App({ Component, pageProps }) {
   return (
@@ -8,6 +9,21 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="google-site-verification" content="l7Wm9u4AZChkT02kada1sN1bddu9oc2b1ChYD2DLmzI" />
       </Head>
+
+      {/* Google Analytics Script */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-RLKNCNHXKR"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-RLKNCNHXKR');
+        `}
+      </Script>
+
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
