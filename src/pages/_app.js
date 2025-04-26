d26882395508cb5375c87c875e9d2ca30d73072d
yastar123 <eduj2164@gmail.com>
2025-04-27 01:10:31 +0700

    commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,34 @@
 import "../styles/globals.css";
-import { AuthProvider } from '../context/AuthContext';
-import Head from 'next/head';
-import Script from 'next/script'; // <-- Tambahkan ini
+import { AuthProvider } from "../context/AuthContext";
+import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="google-site-verification" content="l7Wm9u4AZChkT02kada1sN1bddu9oc2b1ChYD2DLmzI" />
-        <meta name="description" content="Baca komik manga, manhwa, dan manhua terbaru di Komikkuy secara gratis dengan update tiap hari!" />
+        <meta
+          name="google-site-verification"
+          content="l7Wm9u4AZChkT02kada1sN1bddu9oc2b1ChYD2DLmzI"
+        />
+        <meta
+          name="description"
+          content="Baca komik manga, manhwa, dan manhua terbaru di Komikkuy secara gratis dengan update tiap hari!"
+        />
         <link rel="icon" href="/favicon.png" />
+
+        {/* Tambahin Structured Data JSON-LD */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              url: "https://www.komikkuy.my.id",
+              logo: "https://www.komikkuy.my.id/favicon.png",
+            }),
+          }}
+        />
       </Head>
 
       {/* Google Analytics Script */}
